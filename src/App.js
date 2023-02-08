dd6b8e14171579fbb822a33593ab92c47e339534
divinejoshua <divineuche399@gmail.com>
2023-02-08 13:55:17 +0000

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl font-bold underline text-green-500">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
