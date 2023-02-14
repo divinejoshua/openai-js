dfee36718aa728d0eac6707b911fbfb5882fb349
divinejoshua <divineuche399@gmail.com>
2023-02-14 10:07:04 +0000

    Added 1k token

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
         prompt: prompt,
         model: "text-davinci-003",
         temperature: 0,
-        max_tokens: 100,
+        max_tokens: 1000,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
