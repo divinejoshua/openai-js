d0efa2aeff3bd44fc9bfa5495b7400af5f29e5e1
divinejoshua <divineuche399@gmail.com>
2023-03-11 17:09:25 +0000

    Made changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration"; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,12 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorkerRegistration.register();
 
 
 // If you want to start measuring performance in your app, pass a function
